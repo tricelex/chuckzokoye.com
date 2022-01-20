8c562c5db2a0417b09ec6833047bed582818cc31
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-01-20 16:07:27 +0100

    apollo fix 3

--- apollo-client.js
+++ apollo-client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-	uri: process.env.NEXT_PUBLIC_GRAPHCMS_URI,
+	uri: 'https://api-eu-west-2.graphcms.com/v2/cky8ytd9e3bww01z40ev1356q/master',
 	cache: new InMemoryCache(),
 });
