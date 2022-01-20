0f2e8bd056944e331bec7156989f3f3f5a8fcb8a
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-01-20 16:31:51 +0100

    apollo fix 4

--- apollo-client.js
+++ apollo-client.js
@@ -1,6 +1,22 @@
+import { onError } from '@apollo/client/link/error';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-export const client = new ApolloClient({
+const httpLink = new HttpLink({
 	uri: 'https://api-eu-west-2.graphcms.com/v2/cky8ytd9e3bww01z40ev1356q/master',
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors)
+		graphQLErrors.forEach(({ message, locations, path }) =>
+			console.log(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			)
+		);
+
+	if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+export const client = new ApolloClient({
+	link: from([errorLink, httpLink]),
 	cache: new InMemoryCache(),
 });
