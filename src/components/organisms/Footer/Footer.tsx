720733581454cd2ec5f84f296e72e294d7efdc7b
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-07-25 15:44:13 +0100

    fis=x seohead metadata

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,28 +2,28 @@ import { SocialIcons } from 'Molecules/SocialIcons';
 
 const Footer = () => {
 	return (
-		<footer className="flex justify-between mt-8 md:mt-20 px-4 md:px-20 py-8">
-			<span>&copy; {new Date().getFullYear()} Jacob Herper</span>
+		<footer className="flex justify-between px-4 py-8 mt-8 md:mt-20 md:px-20">
+			<span>&copy; {new Date().getFullYear()} Chuckz Okoye</span>
 			<SocialIcons
 				profiles={[
 					{
 						name: 'Github',
-						url: 'https://www.github.com/jakeherp',
+						url: 'https://www.github.com/tricelex',
 						icon: 'GITHUB',
 					},
 					{
 						name: 'LinkedIn',
-						url: 'https://www.linkedin.com/in/jacobherper',
+						url: 'https://www.linkedin.com/in/chuckzokoye',
 						icon: 'LINKEDIN',
 					},
 					{
 						name: 'Twitter',
-						url: 'https://twitter.com/jakeherp',
+						url: 'https://twitter.com/chuckzokoye',
 						icon: 'TWITTER',
 					},
 					{
 						name: 'Instagram',
-						url: 'https://www.instagram.com/jakeherp',
+						url: 'https://www.instagram.com/chuckzokoye',
 						icon: 'INSTAGRAM',
 					},
 				]}
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -33,7 +33,7 @@ const PostPage: NextPage<IProps> = ({
 	return (
 		<AnimatePage>
 			<SeoHead
-				title={`${title} - Jacob Herper's Blog`}
+				title={`${title} - Chuckz Okoye's Blog`}
 				description={seoDescription}
 			/>
 			<Container>
